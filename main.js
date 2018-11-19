489d27ba842379d4f73784760fc734304dd6cdb4
Caitlyn Park <parkcai@oregonstate.edu>
2018-11-19 13:50:48 -0800

    Added chart and Data

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
-
 $(updateView)
 
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+    $.getJSON(BASE_URL +"/rides/count/g:station/per_day_of_year", updatePerDay)
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
+        console.log(data)
+}
+
+function updatePerDay(data) {
+    numberOfRides = data.count
+    $("h3#ridesPerDay").html(numberOfRides)
+        console.log(data)
 }
